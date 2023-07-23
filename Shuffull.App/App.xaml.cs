58b4ec925b76305c1b3f2fdea4f1e23f76e816ed
MC2018 <mc2018.git@gmail.com>
2023-07-23 19:13:18 +0900

    Started maui move, tested new way to play media

---
+++ Shuffull.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Shuffull.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Shuffull.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Shuffull.App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
